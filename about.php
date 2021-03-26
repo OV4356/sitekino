e440c4ad4816e8e45523c7723a2b9221fee99deb
OV4356 <59843317+OV4356@users.noreply.github.com>
2021-03-27 01:47:57 +0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="smart.css" rel="stylesheet">
+		<meta charset="utf-8">
+		<title>Кинотеатр HOLLYWOOD</title>
+		<style>
+			.gradient {
+				background: #fefcea;
+				background: linear-gradient(to top,#778899, #2F4F4F);
+				border-top: 1px solid #333;
+				border-bottom: 1px solid #333;
+				margin-top:0px;
+				margin-bottom:0px;
+				width:100%;
+			}
+		</style>
+		<meta name="keywords" content="кино, фильмы, развлечения, кинотеатр">
+		<meta name="description" content="Кинотеатр  ">
+		<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+		<link rel="shortcut icon" href="star.ico" type="image/x-icon" />
+	</head>
+	
+	<body>
+		<div class="header">	
+			<div>
+			<img width="100%" src="h3.jpg">
+			</div>
+			
+			<ul class="menu gradient"> 
+				<li><a href="tab.php">Главная</a></li>
+				<li><a href="about.php">О кинотеатре</a></li>
+				<li><a href="news.php">Новости и акции</a></li>
+				<li><a href="bue.php">Забронировать</a></li>
+		   </ul>			
+	   </div>
+	  
+		
+		
+		
+			<div class="text"> 				
+			<h1>Вы такого еще не видели!</h1>
+			<p>Мы представляем Вам кинотеатр нового поколения. Ни один кинотеатр города Магнитогорска не сможет предложить то, что есть у нас.
+			Нашем кинотеатре неудобные кресла заменены настоящими мягкими и комфортными кроватями. Теперь не нужно думать о том, как удобнее разместиться,
+			у вас будет достаточно места. Часто бывает так, что хочется сходить в кино, но при этом совсем не хочется вылезать из теплой кровати. 
+			Специально для таких людей придумали инновационный кинозал. Вместо привычных кресел в зале расположены кровати, 
+			где семейные пары или просто друзья могут смотреть фильм в горизонтальном положении. 
+			Всем зрителям дают комплект постельного белья – пододеяльник, подушку и покрывало. 
+			Дизайн зала выполнен в виде обычной гостиной – с напольными лампами, коврами, занавесками и тумбочками. 
+			Именно поэтому создается впечатление, что вы находитесь у себя дома. 
+			<p>Просто посмотрите, как уютно выглядят наши залы:
+			<div class="zal"><figure><img width="50%" src="z1.jpg"><figcaption >Зал 1</figcaption></figure>
+			<br>
+			<figure><img width="50%" src="z2.jpg"><figcaption >Зал 2</figcaption></figure>
+			<br>
+			<figure><img width="50%" src="z3.jpg"><figcaption >Зал 3</figcaption></figure>
+			</div>
+			<br><br><br>
+			
+			
+			
+			
+			
+				</div>
+		
+	
+		<div class="footer gradient">
+			
+
+	
+				<ul class="bottom" style="padding-left:10%">
+					<li><a href="#" style="color: black;  text-transform: none;"> Мы в соцсетях:</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Вконтакте</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Facebook</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Instagram</a></li>
+				</ul>
+		
+				<div class="bottom" style="color: black; padding-left:25%"> 2019 © HOLLYWOOD.ru Все права защищены. 
+				<br>При копировании материалов ссылка на сайт обязательна. </div>
+			
+			</div>
+	
+			
+		</div>
+	</body>
+</html>
