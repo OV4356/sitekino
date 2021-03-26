32bfb0d0f33475847407b58aeb878eb0ea39e395
OV4356 <59843317+OV4356@users.noreply.github.com>
2021-03-27 01:51:20 +0500

    Create tab.php

---
+++ tab.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="smart.css" rel="stylesheet">
+		<meta charset="utf-8">
+		<title>Кинотеатр HOLLYWOOD</title>
+		<style>
+			.gradient {
+				background: #fefcea;
+				background: linear-gradient(to top,#778899, #2F4F4F);
+				border-top: 1px solid #333;
+				border-bottom: 1px solid #333;
+				margin-top:0px;
+				margin-bottom:0px;
+				width:100%;
+			}
+		</style>
+		<meta name="keywords" content="кино, фильмы, развлечения, кинотеатр">
+		<meta name="description" content="Кинотеатр  ">
+		<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+		<link rel="shortcut icon" href="star.ico" type="image/x-icon" />
+	</head>
+	
+	<body>
+		<div class="header">	
+			<div>
+			<img width="100%" src="h3.jpg">
+			</div>
+			
+			<ul class="menu gradient"> 
+				<li><a href="tab.php">Главная</a></li>
+				<li><a href="about.php">О кинотеатре</a></li>
+				<li><a href="news.php">Новости и акции</a></li>
+				<li><a href="bue.php">Забронировать</a></li>
+		   </ul>			
+	   </div>
+	  
+		
+		
+		
+			<div class="text"> 				
+			<h1>Расписание сеансов</h1>
+				<div class="t">
+				<table>
+				<tr><th>
+				<th>пн</th>
+				<th>вт</th>
+				<th>ср</th>
+				<th>чт</th>
+				<th>пт</th>
+				<th>сб</th>
+				<th>вс</th>
+				</tr>
+				<tr><td><img width="150px" src="p1.jpg"></td><td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td></tr>
+				<tr><td><img width="150px" src="p2.jpg"></td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td></tr>
+				<tr><td><img width="150px" src="p3.jpg"></td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td>
+				<td>10:10 <br><br>13:30 <br><br>15:45 <br><br>17:30 <br><br> 19:30 <br><br>20:50 <br><br>22:45</td></tr>
+				
+				</table>
+				
+				
+				</div>
+				
+				
+				
+				
+				
+				</div>
+		
+	
+		<div class="footer gradient">
+			
+
+	
+				<ul class="bottom" style="padding-left:10%">
+					<li><a href="#" style="color: black;  text-transform: none;"> Мы в соцсетях:</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Вконтакте</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Facebook</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Instagram</a></li>
+				</ul>
+		
+				<div class="bottom" style="color: black; padding-left:25%"> 2019 © HOLLYWOOD.ru Все права защищены. 
+				<br>При копировании материалов ссылка на сайт обязательна. </div>
+			
+			</div>
+	
+			
+		</div>
+	</body>
+</html>
