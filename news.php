da136c11393356695bbc020b42bafde5d47d3ce9
OV4356 <59843317+OV4356@users.noreply.github.com>
2021-03-27 01:50:50 +0500

    Create news.php

---
+++ news.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="smart.css" rel="stylesheet">
+		<meta charset="utf-8">
+		<title>Кинотеатр HOLLYWOOD</title>
+		<style>
+			.gradient {
+				background: #fefcea;
+				background: linear-gradient(to top,#778899, #2F4F4F);
+				border-top: 1px solid #333;
+				border-bottom: 1px solid #333;
+				margin-top:0px;
+				margin-bottom:0px;
+				width:100%;
+			}
+		</style>
+		<meta name="keywords" content="кино, фильмы, развлечения, кинотеатр">
+		<meta name="description" content="Кинотеатр  ">
+		<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+		<link rel="shortcut icon" href="star.ico" type="image/x-icon" />
+	</head>
+	
+	<body>
+		<div class="header">	
+			<div>
+			<img width="100%" src="h3.jpg">
+			</div>
+			
+			<ul class="menu gradient"> 
+				<li><a href="tab.php">Главная</a></li>
+				<li><a href="about.php">О кинотеатре</a></li>
+				<li><a href="news.php">Новости и акции</a></li>
+				<li><a href="bue.php">Забронировать</a></li>
+		   </ul>			
+	   </div>
+	  
+		
+		
+		
+			<div class="text"> 				
+			<h1>Для наших посетителей действуют акции</h1>
+			<div class="dis"><img width="50%" src="s1.jpg">
+			<br><br><br><img width="50%" src="s2.png"><br><br>
+			
+			</div>
+				</div>
+		
+	
+		<div class="footer gradient">
+			
+
+	
+				<ul class="bottom" style="padding-left:10%">
+					<li><a href="#" style="color: black;  text-transform: none;"> Мы в соцсетях:</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Вконтакте</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Facebook</a></li>
+					<li><a href="#" style="color: black; text-transform: none;"> Instagram</a></li>
+				</ul>
+		
+				<div class="bottom" style="color: black; padding-left:25%"> 2019 © HOLLYWOOD.ru Все права защищены. 
+				<br>При копировании материалов ссылка на сайт обязательна. </div>
+			
+			</div>
+	
+			
+		</div>
+	</body>
+</html>
